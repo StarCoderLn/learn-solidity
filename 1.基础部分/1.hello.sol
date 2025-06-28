0d208eed54ee6975b1383c5ac4db21642d8491b8
LN <31439591+StarCoderLn@users.noreply.github.com>
2025-06-28 15:01:08 +0800

    Update 1.hello.sol

--- 1.基础部分/1.hello.sol
+++ 1.基础部分/1.hello.sol
@@ -5,10 +5,18 @@ pragma solidity >=0.7.0 <0.9.0;
 import "hardhat/console.sol";
 
 contract HelloWorld {
-    string public Msg;
+    string public greeting;
 
     constructor() {
-        console.log("Hello Solitidy");
-        Msg = "Hello Solitidy";
+        greeting = "Hello World";
+        console.log(greeting);
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
     }
 }
